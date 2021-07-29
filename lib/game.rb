b7bb25930fbd92f217082ed984670eee01a1a6e0
David Bull <david@uk-dave.com>
2021-07-29 16:31:10 +0100

    Implement tests 1 and 2

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,11 @@
 class Game
+  attr_reader :score
 
+  def initialize
+    @score = 0
+  end
+
+  def roll pins
+    @score += pins
+  end
 end
--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -1,5 +1,17 @@
 require "game"
 
 describe Game do
+  context "gutter game" do
+    it "returns 0" do
+      20.times { subject.roll(0) }
+      expect(subject.score).to eq 0
+    end
+  end
 
+  context "all 1's" do
+    it "returns 10" do
+      20.times { subject.roll(1) }
+      expect(subject.score).to eq 20
+    end
+  end
 end
