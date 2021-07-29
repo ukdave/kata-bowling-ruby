4e9c3f81efdb78ef803e3a586c9db02693480f3c
David Bull <david@uk-dave.com>
2021-07-29 16:31:46 +0100

    Implement test 3 (spare)

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,34 @@
 class Game
-  attr_reader :score
-
   def initialize
-    @score = 0
+    @rolls = []
   end
 
   def roll pins
-    @score += pins
+    @rolls << pins
+  end
+
+  def score
+    frame = 0
+    score = 0
+    10.times do
+      score += sum_of_pins_in_frame(frame)
+      score += spare_bonus(frame) if spare?(frame)
+      frame += 2
+    end
+    score
+  end
+
+  private
+
+  def spare? frame
+    sum_of_pins_in_frame(frame) == 10
+  end
+
+  def spare_bonus frame
+    @rolls[frame + 2]
+  end
+
+  def sum_of_pins_in_frame frame
+    @rolls[frame] + @rolls[frame + 1]
   end
 end
--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -14,4 +14,14 @@ describe Game do
       expect(subject.score).to eq 20
     end
   end
+
+  context "one spare" do
+    it "returns 16" do
+      subject.roll(5)
+      subject.roll(5)
+      subject.roll(3)
+      17.times { subject.roll(0) }
+      expect(subject.score).to eq 16
+    end
+  end
 end
